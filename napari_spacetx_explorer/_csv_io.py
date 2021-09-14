870a12eca2c707acf94ac2edd2d813d4e7655d0c
Sebastian Gonzalez Tirado <segonzal@mac-almf15.local>
2021-09-14 13:33:41 +0200

    Solved pd.DataFrame NaN issue

--- napari_spacetx_explorer/_csv_io.py
+++ napari_spacetx_explorer/_csv_io.py
@@ -21,6 +21,7 @@ class CSVIO:
 
     def read(self):
         df = pd.read_csv(self.file_path)
+        df = df.fillna('None')
         #df_gene = df[df['target'] != np.nan]
         #self.data = np.column_stack([df_gene['yc'], df_gene['xc']])
         self.total_data = (np.column_stack([df['yc'], df['xc']]), df['target'])
