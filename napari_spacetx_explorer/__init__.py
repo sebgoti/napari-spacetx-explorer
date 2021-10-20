3c1a8ef66d7caf10b7740ee7e1317d81c0654902
Sebastian Gonzalez Tirado <sebgoti8@gmail.com>
2021-10-20 14:19:20 +0200

    Added _summary.py Fixed __init__

--- napari_spacetx_explorer/__init__.py
+++ napari_spacetx_explorer/__init__.py
@@ -1,11 +1,12 @@
-
 try:
     from ._version import version as __version__
 except ImportError:
-    __version__ = "unknown"
+    __version__ = "0.0.3"
 
 
 from ._reader import napari_get_reader
 from ._function import napari_experimental_provide_function
 from ._dock_widget import napari_experimental_provide_dock_widget
+#from ._summary import napari_experimental_provide_function
 #from ._function import napari_experimental_provide_function
+
